5427d381f015eebdfa6a748f0c4f0f48e6fe78b1
ehlxr <ehlxr.me@gmail.com>
2023-03-06 20:05:59 +0800

    update at 2023-03-06 20:05:59 by ehlxr

--- src/string_match_test.go
+++ src/string_match_test.go
@@ -7,21 +7,21 @@ import (
 
 func TestBf(t *testing.T) {
 	s := "bacbababaabcbab"
-	p := "ababa"
+	p := "abababca"
 
 	fmt.Printf("bf %s match %s index is: %+v\n", p, s, bf(s, p))
 	// t.Logf("%s match %s index is: %+v\n", p, s, bf(s, p))
 }
 
 func TestGetNext(t *testing.T) {
-	p := "ababa"
+	p := "abababca"
 	fmt.Printf("getNext %s next is: %+v\n", p, getNext(p))
 	// t.Logf("%s next is: %+v\n", p, next)
 }
 
 func TestKmp(t *testing.T) {
 	s := "bacbababaabcbab"
-	p := "ababa"
+	p := "abababca"
 
 	fmt.Printf("kmp %s match %s index is: %+v\n", p, s, kmp(s, p))
 	// t.Logf("%s match %s index is: %+v\n", p, s, kmp(s, p))
